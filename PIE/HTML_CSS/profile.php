ee0afabd454ed4e5d11a677bdb6bc753241db95b
Colin Harrison <cs450001_23@CS98148.cs2k3.cwu.edu>
2016-01-13 11:48:29 -0800

    Profile page
    Started the profile page. Currently only contains simple/commented HTML.
    A CSS file for the profile page will be added next. After the CSS, I'll
    work on adding a commented PHP script.

---
+++ PIE/HTML_CSS/profile.php
@@ -0,0 +1,32 @@
+<!DOCTYPE PHP>
+/*
+This file contains the profile page. This page will be populated through information stored in the database.
+Currently this shows a dummy/commented layout that will be written when we add the database to the server.
+*/
+
+<html>
+<head>
+    <title>Profile</title>
+    
+    <link rel="stylesheet" type="text/css" href="template.css">
+	<!-- CSS for the profile page must be written
+    <link rel="stylesheet" type="text/css" href="profile.css">
+	-->
+    <link rel="stylesheet" type="text/css" href="buttons.css">
+    
+	<!-- I will use the pre-written tabs script for tabs for events and other displays on profile pages -->
+    <script src="tabs.js"></script>
+
+    <div id='cssmenu'>
+	    <!-- We will need a different header if we want to keep consistency but have a "logout" button
+        <?php include 'header.php' ?>
+		-->
+    </div>
+	
+    <h1>Welcome back!</h1>
+
+</head>
+
+<!-- The PHP will start below the HTML -->
+
+</html>
