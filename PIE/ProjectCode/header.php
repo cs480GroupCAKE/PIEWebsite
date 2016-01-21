508a47a59bc9eb64801b9f713f53327c0d773700
ZacasaurousRex <riverzac000@hotmail.com>
2016-01-21 11:29:59 -0800

    Updates
    I have updated the header to move the sign up and the login to the right
    side of the page. Also, the sign up and log in have a bar separating
    them now. In the template I moved the old code to the bottom of the page
    so that it is easier to get to the code that is needed.

--- PIE/ProjectCode/header.php
+++ PIE/ProjectCode/header.php
@@ -2,14 +2,13 @@
 <html>
     <head>
         <ul>
-             <li><a href="home.php">Home</a></li>
-             <li><a href="about.php">About</a></li>
-             <li><a href="helpPage.php">Help</a></li>
-             <li><a href="signUp.php">Register</a></li>
-             <li><a href="loginPage.php">Login</a></li>
-
-
+            <li><a href="home.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="helpPage.php">Help</a></li>
+            <ul>
+			    <li style="border-right: 1px solid #00b7e5"><a href="signUp.php">Sign Up</li>
+				<li><a href="loginPage.php">Login</li>
+            </ul>
         </ul>
-
     </head>
 </html>
