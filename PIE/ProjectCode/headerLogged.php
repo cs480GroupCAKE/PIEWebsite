b26d53be51254b44ac4ed8e235e479826b26edc1
ZacasaurousRex <riverzac000@hotmail.com>
2016-01-25 14:25:36 -0800

    Added a header logged and events.php
    The header logged is used as the navigation bar after a user has logged
    in. The home button takes them to the profile page and there is a sign
    out button that needs to sign a user out, but takes you to the home
    page.

---
+++ PIE/ProjectCode/headerLogged.php
@@ -0,0 +1,17 @@
+<!DOCTYPE PHP>
+<html>
+    <head>
+        <ul>
+            <li><a href="profile.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="helpPage.php">Help</a></li>
+            
+            <!--TODO: MAKE SURE TO HAVE THE SIGN OUT BUTTON ACTUALLY SIGN USERS OUT Z.R. 1/25/16-->
+            <ul style="float:right; list-style-type:none;">
+                <li><a href="home.php">Sign Out</a></li>
+            </ul>
+
+        </ul>
+
+    </head>
+</html>
