3ac78b1cefdcc176b6f31c61dff4037686ba4f72
ZacasaurousRex <riverzac000@hotmail.com>
2016-01-25 13:54:47 -0800

    Reformatting...again
    Properly reformatted the header. Had a bug in the previous push, but now
    the navigation bar is properly aligned.

--- PIE/ProjectCode/header.php
+++ PIE/ProjectCode/header.php
@@ -5,9 +5,10 @@
              <li><a href="home.php">Home</a></li>
              <li><a href="about.php">About</a></li>
              <li><a href="helpPage.php">Help</a></li>
-             <li><a href="signUp.php">Register</a></li>
-             <li><a href="loginPage.php">Login</a></li>
-
+            <ul style="float:right; list-style-type:none;">
+                <li style="border-right: 1px solid #00b7e5"><a href="signUp.php">Register</a></li>
+                <li><a href="loginPage.php">Login</a></li>
+            </ul>
 
         </ul>
 
