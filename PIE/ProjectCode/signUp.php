b9dac38ca32ccb7df2b9f4c04f2b74f76c785978
Colin Harrison <almostcolin@gmail.com>
2016-02-01 14:57:22 -0800

    Revert "Moved Images folder into ProjectCode"
    This reverts commit decdb8ae89ecdb5d6a3102f1a876dcb4ddf144a6.

--- PIE/ProjectCode/signUp.php
+++ PIE/ProjectCode/signUp.php
@@ -21,13 +21,14 @@ only a few are to make testing database code easier. Should catch handle and pri
     <script>
         $(function() {
             $( "#datepicker" ).datepicker({
-                changeMonth: true,
-                changeYear: true
+            changeMonth: true,
+            changeYear: true
             });
-            
-            $( ".selector" ).datepicker( "option", "yearRange", "2002:2012" );
         });
-        
+        for(i= new Date().getFullYear(); i>1900; i--)
+        {
+            $ ('#datepicker').append($('<option/>').val(i).html(i));
+        }
     </script>
             
     <div id='cssmenu'>
