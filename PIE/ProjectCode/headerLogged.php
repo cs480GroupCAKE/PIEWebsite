a9c07d2892ca651f6bbf5aadfd68bcef4a1ac055
Colin Harrison <almostcolin@gmail.com>
2016-02-01 17:15:21 -0800

    Added to logged header
    Added search for connections to the logged header. PHP for the search
    form still needs to be made.

--- PIE/ProjectCode/headerLogged.php
+++ PIE/ProjectCode/headerLogged.php
@@ -8,11 +8,16 @@
             
             <!--TODO: MAKE SURE TO HAVE THE SIGN OUT BUTTON ACTUALLY SIGN USERS OUT Z.R. 1/25/16-->
             <ul style="float:right; list-style-type:none;">
-                <li><font color="white" style="padding-top:10px">Search for Connections: </font></li>
                 <li style="border-left: 1px solid #ffffff"><a href="signout.php">Sign Out</a></li>
             </ul>
 
+            <font color="white" style="padding-right:130px;">Search for Connections:</font>
+            <form action="searchUser.php" method="post">
+                <input type="text" name="searchedUser">
+                <input type="submit" value="Search" name="submit"><font style="padding-right:130px;"></font>
+            </form>
+            
         </ul>
-
+        
     </head>
 </html>
