150d2a97981d47a72fb64b35eaa6079c188522f0
timosteppa <nakhisat@cwu.edu>
2016-02-10 15:19:21 -0800

    slider for events

--- PIE/ProjectCode/events.php
+++ PIE/ProjectCode/events.php
@@ -79,8 +79,51 @@
         }
 
     </script>    
+	
+	<!--				Slider for events. TN 2/10/16 							
+	
+	
+	
+	
+	-->
+	
+	
+	<style>
+    /* Prevents slides from flashing */
+    #slides {
+      display:none;
+    }
+	</style>
 
+	<script src="http://code.jquery.com/jquery-latest.min.js">
+	</script>
+	<script src="jquery.slides.min.js"></script>
+
+	<script>
+		$(function(){
+		  $("#slides").slidesjs({
+			width: 940,
+			height: 528
+		  });
+		});
+    </script>
+	
 </head>
+
+<body>
+	<div id="slides">
+		<a href="http://www.cwu.edu/publicity/reuniting-families-torn-apart-incarceration-mothers-day-california-prisons">
+		<img src="http://placehold.it/940x528">
+		</a>	
+		<img src="http://placehold.it/940x528">
+		<img src="http://placehold.it/940x528">
+		<img src="http://placehold.it/940x528">
+		<img src="http://placehold.it/940x528">
+	
+	</div>
+<!-- End of the slider -->
+</head>
+
 <body>
     <form action="./background/createEvent.php" method="post">
 
