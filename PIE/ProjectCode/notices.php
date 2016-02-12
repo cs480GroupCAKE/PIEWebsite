0ed6868de8937de466c390b02b7f7e9ae8fee380
mcmurtrie <mcmurtriec@cwu.edu>
2016-02-12 13:59:31 -0800

    created notices page to display notifications

---
+++ PIE/ProjectCode/notices.php
@@ -0,0 +1,32 @@
+<DOCTYPE! PHP>
+<?php
+/*
+KEEP IN CODE AND COMMENTED OUT UNLESS DEBUGGING
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('dispaly_startup_errors', '1');
+echo ini_get('display_errors');
+*/
+?>
+<?php
+    //GLOBAL VARIABLES
+?>
+
+<html>
+<head>
+    <title>Notices</title>
+    <link rel="stylesheet" type="text/css" href="./stylesheets/template.css">
+    <div id='cssmenu'>
+        <?php require './background/loggedcheck.php';?>
+    </div>
+</head>
+<body>
+    
+</body>
+
+
+    <div id='footer'>
+        <? php include './templates/footer.php'?>
+    </div>
+</html>
