f5dc4a0ff73d6ef55c96317ce6c72b47e20bdc31
Colin Harrison <almostcolin@gmail.com>
2016-02-12 13:59:52 -0800

    Added divs for editPhotos
    Changed to new coding standard

--- PIE/ProjectCode/editPhotos.php
+++ PIE/ProjectCode/editPhotos.php
@@ -15,7 +15,21 @@
 
 <body>
 <!--    <form action="./background/createEvent.php" method="post">  -->
+<div id='container'>
+    <div id='header'>
+        <div id='cssmenu'>
+            <?php include'./templates/headerLogged.php'?>
+        </div>
+    </div>
+    
+    <div id='divCenter'>
 
 
+    </div>
+    
+    <div id='footer'>
+        <?php include'./templates/footer.php'?>
+    </div>
+</div>
 </body>
 </html>
